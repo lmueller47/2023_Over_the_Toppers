eab98b7a9a8b293dbe2a0c661b73f166f87d03a7
JordanAC03 <147213848+JordanAC03@users.noreply.github.com>
2023-10-06 20:49:49 -0500

    move

---
+++ Over The Toppers/Assets/Scripts/GameManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static int stackCount;
+    // Start is called before the first frame update
+    void Start()
+    {
+        stackCount = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //change pizza player speed value based on topping stackCount
+
+        //timer for corperate inspection
+    }
+}
