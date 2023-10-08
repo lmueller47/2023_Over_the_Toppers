a595b2969765d26a72d30ff2a6b03e91e360dd57
lmueller47 <logan122902@gmail.com>
2023-10-08 13:23:18 -0500

    bug fix
    Object shooting to way negative space.

--- Over The Toppers/Assets/Scripts/Stacking.cs
+++ Over The Toppers/Assets/Scripts/Stacking.cs
@@ -13,11 +13,9 @@ public class Stacking : MonoBehaviour
     public static bool death = false;
 
     public float speed = 20f;
-    private Rigidbody rb;
 
     private void Start()
     {
-        rb = GetComponent<Rigidbody>();
         GameManager.dirtyCount += 1;
         maker = GameObject.Find("Maker");
     }
@@ -30,14 +28,15 @@ public class Stacking : MonoBehaviour
             {
                 {
 
-                    //set this object as a child of the pizza object
-                        this.gameObject.transform.parent = collision.gameObject.transform;
+                //set this object as a child of the pizza object
+                gameObject.layer = collision.gameObject.layer;
+                this.gameObject.transform.parent = collision.gameObject.transform;
+                this.gameObject.tag = "Player";
 
 
-                        //set this objects tage to player
-                        MakerMovement.toppings.Add(gameObject);
+                //set this objects tage to player
+                MakerMovement.toppings.Add(gameObject);
                         player = true;
-                        gameObject.layer = collision.gameObject.layer;
                         gameObject.transform.position = new Vector3(collision.gameObject.transform.position.x, gameObject.transform.position.y, collision.gameObject.transform.position.z);
 
                     //add to stack count
@@ -63,6 +62,10 @@ public class Stacking : MonoBehaviour
         if(player)
         {
             this.gameObject.tag = "Player";
+            if(gameObject.transform.position.y < maker.transform.position.y)
+            {
+                gameObject.transform.position = new Vector3(maker.gameObject.transform.position.x, 4, maker.gameObject.transform.position.z);
+            }
             gameObject.transform.position = new Vector3(maker.gameObject.transform.position.x, gameObject.transform.position.y, maker.gameObject.transform.position.z);
         }
     }
